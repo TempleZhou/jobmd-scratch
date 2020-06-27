73728c5e6d2d274d006cb7471c621b8c4c2a2a06
zhoutianpeng <ztp@apache.org>
2020-06-27 11:35:59 +0800

    移除废弃代码

--- Jobmd/run.py
+++ Jobmd/run.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-import os
-import re
 import sys
+
 from scrapy.cmdline import execute
+
 if __name__ == '__main__':
-    os.environ['DATA_DIR'] = os.path.abspath(os.curdir) + "/JsonData"
-    try:
-        os.mkdir(os.environ.get("DATA_DIR"))
-    except FileExistsError:
-        pass
-    print(f'数据目录为：{os.environ.get("DATA_DIR")}')
     sys.argv = [sys.argv[0], "crawl", "jobmd"]
     sys.exit(execute())
